bfbcda1343db7c3ffee204b4a0a8b6e27f393e90
ofek29 <ofek.av95@gmail.com>
2024-10-23 23:24:33 +0300

    register

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,15 @@
 
 export const Login = () => {
     return (
-        <div>Login</div>
+        <div className="justify-center flex bg-neutral-400 h-dvh ">
+            <form className="w-80  gap-8 flex flex-col justify-center ">
+                <h1 className=" text-xl text-center">Sign In</h1>
+
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button type="submit" className="rounded-lg bg-slate-500 hover:bg-orange-300">Login</button>
+
+            </form>
+        </div >
     )
 }
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,15 @@
-
 export const Register = () => {
     return (
-        <div>register</div>
+        <div className="justify-center flex bg-neutral-400 h-dvh ">
+            <form className="w-80  gap-8 flex flex-col justify-center al ">
+                <h1 className=" text-xl text-center">Sign Up</h1>
+
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button type="submit" className="rounded-lg bg-slate-500 hover:bg-orange-300">Register</button>
+
+            </form>
+        </div >
     )
 }
